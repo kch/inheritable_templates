f9ac1a0e03354d0f6beb44cc6b66c41fcf5a6735
Caio Chassot <dev@caiochassot.com>
2009-02-08 20:32:29 -0200

    make compatible with rails 2.3 edge at commit 858a420ce18719c720b80508b336e37ce37a20bf

--- lib/inheritable_templates.rb
+++ lib/inheritable_templates.rb
@@ -1,11 +1,20 @@
 module InheritableTemplates
+  
+  def self.template_exists?(controller, template_name)
+    begin
+      controller.view_paths.find_template(template_name, controller.send(:default_template_format))
+      true
+    rescue ActionView::MissingTemplate
+      false
+    end
+  end
 
   # abstracts the process of finding an existing template up the controller inheritance chain
   def self.find_inherited_template(controller, template_basename)
     k = controller.class
     while (k = k.superclass) < ActionController::Base
       template_name = "#{k.controller_path}/#{template_basename}"
-      return template_name if controller.send :template_exists?, template_name
+      return template_name if template_exists?(controller, template_name)
     end
   end
 
@@ -17,7 +26,7 @@ module InheritableTemplates
   
     def default_template_name_with_inheritance(action_name = self.action_name)
       template_name = default_template_name_without_inheritance(action_name)
-      return template_name if template_exists? template_name
+      return template_name if InheritableTemplates.template_exists?(self, template_name)
       InheritableTemplates.find_inherited_template(self, action_name) || template_name
     end
   end
@@ -29,13 +38,14 @@ module InheritableTemplates
     end
 
     def _pick_partial_template_with_inheritance(partial_path)
-      _pick_template case
+      path = case
       when partial_path.include?('/') then File.join(File.dirname(partial_path), "_#{File.basename(partial_path)}")
       when respond_to?(:controller) then
-        controller.send(:template_exists?, (p = "#{controller.class.controller_path}/_#{partial_path}")) && p ||
-          InheritableTemplates.find_inherited_template(controller, "_#{partial_path}")
+        InheritableTemplates.template_exists?(controller, p = "#{controller.class.controller_path}/_#{partial_path}") ?
+          p : InheritableTemplates.find_inherited_template(controller, "_#{partial_path}")
       else "_#{partial_path}"
       end
+      self.view_paths.find_template(path, self.template_format)
     end
   end
   
